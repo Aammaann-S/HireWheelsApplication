96f081ed4125ff8857a0b2ff165256634fa03181
Aman <90821327+Aammaann-S@users.noreply.github.com>
2023-10-17 09:55:17 +0530

    Delete HireWheelsApplication directory

--- HireWheelsApplication/src/main/java/org/ncu/hirewheels/dao/CityDao.java
+++
@@ -1,8 +0,0 @@
-package org.ncu.hirewheels.dao;
-
-import org.ncu.hirewheels.entities.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CityDao  extends JpaRepository<City, Long>{
-
-}
--- HireWheelsApplication/src/main/java/org/ncu/hirewheels/dao/VehicleCategoryDao.java
+++
@@ -1,9 +0,0 @@
-package org.ncu.hirewheels.dao;
-
-import org.ncu.hirewheels.entities.VehicleCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleCategoryDao extends JpaRepository<VehicleCategory, Long>{
-
-	public VehicleCategory findByVehicleCategoryId(long vehicleCategoryId);
-}
